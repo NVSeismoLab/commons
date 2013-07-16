94340ed7a88a868b2d277951a23f871c3e7dde9e
Mark Williams <mark.c.will@gmail.com>
2013-07-16 16:50:55 -0700

    Made mods for CustomConverter name change

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 #
 # make some stuff available at the package level
 #
-from nsleventbuilder import NSLEventBuilder
 from csseventconverter import CSSEventConverter
+from customconverter import CustomConverter
 from util import __antelopeversion__, CharPkt, pfgetter, azimuth2compass
-import psycods2 as dbapi2
+import psycods2 as dbapi2 # change to curds2 or remove, curds2 is now external dep
 import quakeml
