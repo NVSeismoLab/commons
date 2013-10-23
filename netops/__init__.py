8ce24043502e34e3508b11f012b0ebf4a3315530
Mark Williams <mark.c.will@gmail.com>
2013-10-23 14:33:53 -0700

    Version bump for pip

--- netops/__init__.py
+++ netops/__init__.py
@@ -1,7 +1,7 @@
 #
 # make some stuff available at the package level
 #
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 
 from converters import *
 from util import __antelopeversion__, CharPkt, pfgetter, azimuth2compass
