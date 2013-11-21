211c89f393e377b1f0d41dc8c3f6712a30e0a208
Mark Williams <mark.c.will@gmail.com>
2013-11-21 10:26:46 -0800

    Version bump 0.4.4 for loc code bugfix

--- netops/__init__.py
+++ netops/__init__.py
@@ -1,7 +1,7 @@
 #
 # make some stuff available at the package level
 #
-__version__ = '0.4.3'
+__version__ = '0.4.4'
 
 from converters import *
 from util import __antelopeversion__, CharPkt, pfgetter, azimuth2compass
