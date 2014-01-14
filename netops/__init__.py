b892e75baaa8cb08cec39c0babd6c7491652b927
Mark Williams <mark.c.will@gmail.com>
2014-01-14 11:53:06 -0800

    version bump to ..9 for minor bugfixes

--- netops/__init__.py
+++ netops/__init__.py
@@ -1,7 +1,7 @@
 #
 # make some stuff available at the package level
 #
-__version__ = '0.4.7'
+__version__ = '0.4.9'
 
 from converters import *
 from util import __antelopeversion__, CharPkt, pfgetter, azimuth2compass
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from numpy.distutils.core import setup
 ### Regular setup stuff ######################################################
 
 s_args = {'name'         : 'netops',
-          'version'      : '0.4.7',
+          'version'      : '0.4.9',
           'description'  : 'Network Operations utilities',
           'author'       : 'Nevada Seismological Lab',
           'url'          : 'https//github.com/NVSeismoLab',
