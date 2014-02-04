1f95e3c32266805078bd472c0ae480105debbb17
Mark Williams <mark.c.will@gmail.com>
2014-02-04 11:04:07 -0800

    Version to 0.5.3 for netcode bugfix

--- netops/__init__.py
+++ netops/__init__.py
@@ -1,7 +1,7 @@
 #
 # make some stuff available at the package level
 #
-__version__ = '0.5.2'
+__version__ = '0.5.3'
 
 from converters import *
 from util import __antelopeversion__, CharPkt, pfgetter, azimuth2compass
