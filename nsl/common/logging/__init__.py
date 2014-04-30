2d72f42307b21ec895f581e5030f053606ac82df
Mark <mark.c.will@gmail.com>
2014-04-30 10:28:17 -0700

    Tried to make 2.6-compat with stub dictConfig fxn

--- nsl/common/logging/__init__.py
+++ nsl/common/logging/__init__.py
@@ -9,7 +9,33 @@ from logging import *
 try:
     from logging.config import dictConfig
 except ImportError:
-    pass
+    def dictConfig(config):
+        """
+        Backward compatible incomplete replacement for logging.config.dictConfig
+        (for Python 2.6)
+        """
+        # Works for the current settings in customConfig only!!!
+        # -- For any changes compatibility MUST be added to this as well
+        # TODO: implement streams using 'ext://' url notation
+        for logname in config.get('loggers', []):
+            logconfig = config['loggers'][logname]
+            logger = getLogger(logname)
+            logger.setLevel(logconfig.get('level',0))
+            logger.handlers = []
+            for hndname in logconfig.get('handlers', []):
+                hndconfig = config['handlers'][hndname]
+                classname = hndconfig.get('class').strip('logging.')
+                formatname = hndconfig.get('formatter')
+                hndlevel = hndconfig.get('level', 0)
+                class_ = globals()[classname]
+                hnd = class_()
+                hnd.setLevel(hndlevel)
+                if formatname:
+                    fmtconfig = config['formatters'][formatname]
+                    formatter = Formatter(fmtconfig.get('format'))
+                    hnd.setFormatter(formatter)
+                logger.handlers.append(hnd)
+
 
 DEFAULT_LOG_LEVEL = INFO
 DEFAULT_LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
@@ -54,11 +80,18 @@ def customLogger(name='root', handlers=['null'], level=DEFAULT_LOG_LEVEL):
     """
     Return custom logger built with customConfig function
     """
-    #TODO: make backwards compat with 2.6
-    # (parse config manually and add stuff to logger)
     dictConfig(customConfig(name, handlers, level))
     return getLogger(name)
 
+
+class LoggedType(type):
+    """
+    Metaclass, adds an initialized logger instance
+    """
+    def __new__(cls, name, bases, dict_):
+        dict_['logger'] = customLogger(name)
+        return super(LoggedType, cls).__new__(type, name, bases, dict_)
+        
 
 #--- Python 2.6 compat -----------------------------------------------#
 class NullLogger(object):
@@ -74,13 +107,4 @@ class NullLogger(object):
             except:
                 raiseExceptions = False
         return logger
-
-
-class LoggedType(type):
-    """
-    Metaclass, adds an initialized logger instance
-    """
-    def __new__(cls, name, bases, dict_):
-        dict_['logger'] = NullLogger(name)
-        return super(LoggedType, cls).__new__(type, name, bases, dict_)
-        
+#---------------------------------------------------------------------#
